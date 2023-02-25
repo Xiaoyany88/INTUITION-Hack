f24823703b75d3bfe2e12268ba6d7bbaa55bc278
Xiaoyany88 <104213111+Xiaoyany88@users.noreply.github.com>
2023-02-25 12:18:45 +0800

    pushed

--- pdfreader.py
+++ pdfreader.py
@@ -1,5 +1,5 @@
 import PyPDF2
-
+import re
 # Open the PDF file in read-binary mode
 with open('Research article sample.pdf', 'rb') as pdf_file:
     
@@ -20,3 +20,44 @@ with open('Research article sample.pdf', 'rb') as pdf_file:
         
 # Print the extracted text
 print(text)
+
+filename = "output.txt"
+
+with open(filename, mode="w", encoding='utf-8') as file:
+    # write text string into file
+    file.write(text)
+
+# Read in the text file
+with open('output.txt', 'r', encoding='utf-8') as file:
+    text = file.read()
+# Remove any text in square brackets (e.g. references)
+text = re.sub(r'\[[^\]]*\]', '', text)
+
+# Remove any text in curly braces (e.g. metadata)
+text = re.sub(r'\{[^\}]*\}', '', text)
+
+# Remove any footnotes
+text = re.sub(r'\n\d+\.\s[^\n]*', '', text)
+
+# Remove any URLs or email addresses
+text = re.sub(r'\bhttps?:\/\/[^\s]+', '', text)
+text = re.sub(r'\b[\w\.-]+@[\w\.-]+\.[\w]+', '', text)
+
+# Remove any remaining punctuation and whitespace
+text = re.sub(r'[^\w\s]', '', text)
+text = re.sub(r'\s+', ' ', text)
+
+# Remove metadata
+#metadata = re.search(r'(abstract|keywords|introduction)\W[\w\W]*', text, flags=re.I)
+#if metadata:
+#    text = text.replace(metadata.group(), '')
+
+# Remove any remaining whitespace and newlines
+##text = re.sub(r'\s+', ' ', text)
+
+# Save the cleaned text to a new file
+with open('cleaned_output.txt', 'w', encoding='utf-8') as f:
+    f.write(text)
+
+
+
