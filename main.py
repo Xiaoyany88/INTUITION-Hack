6b50230dd2658660aec682a73e859b45018e561c
Denzyl <denz0003@e.ntu.edu.sg>
2023-02-25 13:08:15 +0800

    updated main.py

--- main.py
+++ main.py
@@ -13,23 +13,16 @@ if __name__ == '__main__':
     href = link.get('href')
     filename = href.split('/')[-1]
     newLink = "https" + href[3:]
-    print(newLink)
-    print(filename)
-    file_path = "/Users/darren/PycharmProjects/INTUITION-Hack/database/" + filename
-    print(file_path)
+    # print(newLink)
+    # print(filename)
+    # file_path = "/Users/darren/PycharmProjects/INTUITION-Hack/database/" + filename
+    file_path = "database/" + filename
+    # print(file_path)
     response2 = requests.get(newLink)
-    print(href)
+    # print(href)
     pdf = open(file_path, 'wb')
     pdf.write(response2.content)
     pdf.close()
-    # print(response2)
-
-
-    # open_url()
-    # add to database directory
-    # with open(file_path, 'w') as f:
-    #     f.write(href)
-        # f.write(response.content)
     print("Done")
 
 
