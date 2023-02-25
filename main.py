523d8cf74d572d20d2f8d894ef368333550cc166
Denzyl <denz0003@e.ntu.edu.sg>
2023-02-25 13:13:45 +0800

    updated main.py

--- main.py
+++ main.py
@@ -9,19 +9,20 @@ if __name__ == '__main__':
     response = requests.get(URL)
     soup = BeautifulSoup(response.content, 'html.parser')
     # for link in soup.findAll('link'):
-    link = soup.find('link')
-    href = link.get('href')
-    filename = href.split('/')[-1]
-    newLink = "https" + href[3:]
-    # print(newLink)
-    # print(filename)
-    file_path = "database/" + filename
-    # print(file_path)
-    response2 = requests.get(newLink)
-    # print(href)
-    pdf = open(file_path, 'wb')
-    pdf.write(response2.content)
-    pdf.close()
+    links = soup.findAll('link')[0:5]
+    for link in links:
+        href = link.get('href')
+        filename = href.split('/')[-1]
+        newLink = "https" + href[3:]
+        # print(newLink)
+        # print(filename)
+        file_path = "database/" + filename
+        # print(file_path)
+        response2 = requests.get(newLink)
+        # print(href)
+        pdf = open(file_path, 'wb')
+        pdf.write(response2.content)
+        pdf.close()
     print("Done")
 
 
