9237907d3d788ef54deb051403131a9eee1261c2
Denzyl <denz0003@e.ntu.edu.sg>
2023-02-25 13:17:54 +0800

    moved code from main.py to extractPDF.py

--- main.py
+++ main.py
@@ -1,29 +1,10 @@
-import requests
-from bs4 import BeautifulSoup
-
+from extractPDF import *
 
 URL = "https://www.ncbi.nlm.nih.gov/pmc/utils/oa/oa.fcgi?from=2023-01-01&format=pdf"
 
 if __name__ == '__main__':
     
-    response = requests.get(URL)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    # for link in soup.findAll('link'):
-    links = soup.findAll('link')[0:5]
-    for link in links:
-        href = link.get('href')
-        filename = href.split('/')[-1]
-        newLink = "https" + href[3:]
-        # print(newLink)
-        # print(filename)
-        file_path = "database/" + filename
-        # print(file_path)
-        response2 = requests.get(newLink)
-        # print(href)
-        pdf = open(file_path, 'wb')
-        pdf.write(response2.content)
-        pdf.close()
-    print("Done")
+    extract(URL)
 
 
 
