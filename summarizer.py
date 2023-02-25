ae4139b964a53e01240a7336f52e04b7c574f2e2
Xiaoyany88 <104213111+Xiaoyany88@users.noreply.github.com>
2023-02-25 14:03:51 +0800

    summarizer done

---
+++ summarizer.py
@@ -0,0 +1,54 @@
+import openai
+import wget
+import pathlib
+import pdfplumber
+import numpy as np
+
+def getPaper(paper_url, filename="random_paper1.pdf"):
+    """
+    Downloads a paper from it's arxiv page and returns
+    the local path to that file.
+    """
+    downloadedPaper = wget.download(paper_url, filename)    
+    downloadedPaperFilePath = pathlib.Path(downloadedPaper)
+
+    return downloadedPaperFilePath
+
+# paperFilePath = "Research article sample.pdf"
+paper_url = "https://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_pdf/fe/ae/nihms-1849852.PMC9805511.pdf"
+paperFilePath = getPaper(paper_url)
+paperFilePath = "./random_paper1.pdf"
+paperContent = pdfplumber.open(paperFilePath).pages
+
+def displayPaperContent(paperContent, page_start=0, page_end=5):
+    for page in paperContent[page_start:page_end]:
+        print(page.extract_text())
+#displayPaperContent(paperContent)
+    
+def showPaperSummary(paperContent):
+    tldr_tag = "\n tl;dr:"
+    # openai.organization = 'organization key'
+    openai.api_key = "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"
+    engine_list = openai.Engine.list() # calling the engines available from the openai api 
+    
+    for page in paperContent:    
+        text = page.extract_text() + tldr_tag
+        response = openai.Completion.create(engine="davinci",prompt=text,temperature=0.3,
+            max_tokens=140,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=["\n"]
+        )
+        print(response["choices"][0]["text"])
+        generated_text = response["choices"][0]["text"]
+        with open("summary.txt", "a") as file:
+            file.write(generated_text + "\n")
+            file.write("\n---\n\n")
+        
+
+    
+
+
+showPaperSummary(paperContent)
+
