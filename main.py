ea74e44a6f789ffa5dcf749e8bb3fe55fa15627d
darrennjk <darrennjk@gmail.com>
2023-02-25 12:55:54 +0800

    Update

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 
 
 URL = "https://www.ncbi.nlm.nih.gov/pmc/utils/oa/oa.fcgi?from=2023-01-01&format=pdf"
-file_path = "/database"
 
 if __name__ == '__main__':
     print("hello world");
@@ -13,8 +12,24 @@ if __name__ == '__main__':
     # for link in soup.findAll('link'):
     link = soup.find('link')
     href = link.get('href')
+    filename = href.split('/')[-1]
+    print(filename)
+    file_path = "/Users/darren/PycharmProjects/INTUITION-Hack/database/" + filename
+    print(file_path)
+    response2 = requests.get(href)
     print(href)
-    open("downloads/articles", "wb").write(href)
+    pdf = open(file_path, 'wb')
+    pdf.write(response2.content)
+    pdf.close()
+    # print(response2)
+
+
+    # open_url()
+    # add to database directory
+    # with open(file_path, 'w') as f:
+    #     f.write(href)
+        # f.write(response.content)
+    print("Done")
 
 
 
